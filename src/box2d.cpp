e1a100255b38e8b933caf56f5cc70a7a5469513d
SleepyDude <ko_on@mail.ru>
2016-10-05 13:46:30 +0300

    rename rectangle to box2d

--- src/box2d.cpp
+++ src/box2d.cpp
@@ -1,28 +1,28 @@
-#include "rectangle.h"
+#include "box2d.h"
 
 
 
-Rectangle::Rectangle(Point2D p1, Point2D p2)
+Box2D::Box2D(Point2D p1, Point2D p2)
   : m_p1(p1), m_p2(p2)
 {}
-Rectangle::Rectangle(Rectangle const & obj)
+Box2D::Box2D(Box2D const & obj)
   : m_p1(obj.m_p1), m_p2(obj.m_p2)
 {}
-Rectangle::Rectangle(float f1, float f2, float f3, float f4)
+Box2D::Box2D(float f1, float f2, float f3, float f4)
   : m_p1(f1,f2), m_p2(f3,f4)
 {}
 
-Point2D & Rectangle::p1() { return m_p1; }
-Point2D & Rectangle::p2() { return m_p2; }
+Point2D & Box2D::p1() { return m_p1; }
+Point2D & Box2D::p2() { return m_p2; }
 
 // Оператор логического равенства.
-bool Rectangle::operator == (Rectangle const & obj) const
+bool Box2D::operator == (Box2D const & obj) const
 {
   return (m_p1 == obj.m_p1)&&(m_p2 == obj.m_p2);
 }
 
 // Оператор присваивания.
-Rectangle & Rectangle::operator = (Rectangle const & obj)
+Box2D & Box2D::operator = (Box2D const & obj)
 {
   if (this == &obj) return *this;
   m_p1 = obj.m_p1;
@@ -31,20 +31,20 @@ Rectangle & Rectangle::operator = (Rectangle const & obj)
 }
 
 // Оператор логического неравенства.
-bool Rectangle::operator != (Rectangle const & obj) const
+bool Box2D::operator != (Box2D const & obj) const
 {
-  return !Rectangle::operator==(obj);
+  return !Box2D::operator==(obj);
 }
 
 // Переопределение оператора [].
-Point2D Rectangle::operator [] (unsigned int index) const
+Point2D Box2D::operator [] (unsigned int index) const
 {
   if (index >= 2) return {0.0f, 0.0f};
   return index == 0 ? m_p1 : m_p2;
 }
 
 // Оператор проверки пересечения
-bool Rectangle::operator % (Rectangle const & obj)
+bool Box2D::operator % (Box2D const & obj)
 {
   Point2D nill;
   bool b1 = obj.m_p1 - m_p2 <<= nill,
---
+++ src/box2d.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <ostream>
+#include <cmath>
+#include "point2d.hpp"
+
+
+class Box2D
+{
+public:
+  Box2D() = default;
+  Box2D(Point2D,Point2D);
+  Box2D(Box2D const &);
+  Box2D(float,float,float,float);
+
+  bool operator == (Box2D const &) const;
+  bool operator != (Box2D const &) const;
+  Point2D operator [] (unsigned int index) const;
+  bool operator % (Box2D const &);
+
+  Point2D & p1();  Point2D & p2();
+
+  Box2D & operator = (Box2D const &);
+private:
+
+  Point2D m_p1 = { 0.0f, 0.0f }, m_p2 = { 1.0f, 1.0f };
+};
